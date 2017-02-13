6c82420a959aab4225b0d6f4816359140a1c4c9d
Alen-Nikolov <alennikolov95@gmail.com>
2017-02-13 19:05:49 +0200

    First task completed

--- Task1.js
+++ Task1.js
@@ -1,6 +1,29 @@
-function Task(){
+function Task(name, workingHours){
+    this.name=name;
+    this.workingHours=workingHours;
 
 }
-function Employee(){
-    
+function Employee(name, currentTask,hoursLeft){
+    this.name=name;
+    this.currentTask=currentTask;
+    this.hoursLeft=hoursLeft;
+    this.work=function(){
+        if(currentTask.workingHours>=this.hoursLeft){
+            currentTask.workingHours-=this.hoursLeft;
+            this.hoursLeft=0;
+        } else{
+            this.hoursLeft-=currentTask.workingHours;
+            currentTask.workingHours=0;
+        }
+    },
+    this.showReport=function(){
+        console.log("Ime: "+this.name);
+        console.log("Zadacha: "+currentTask.name);
+        console.log("Ostavashti chasove na rabotnika: "+this.hoursLeft);
+        console.log("Ostavashti chasove ot zadachata: "+currentTask.workingHours)
+    }
 }
+var task=new Task("miene",7);
+var p=new Employee("Taskomir",task,8 );
+p.work();
+p.showReport();
