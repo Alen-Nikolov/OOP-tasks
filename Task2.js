aac6d85dcfed6f313f59c13966dfb5851cd07320
Alen-Nikolov <alennikolov95@gmail.com>
2017-02-17 20:17:56 +0200

    Last commit for task 2

--- Task2.js
+++ Task2.js
@@ -5,22 +5,19 @@ function Computer(year, price,isNotebook, hardDiskMemory,freeMemory, operatingSy
     this.hardDiskMemory=hardDiskMemory;
     this.freeMemory=freeMemory;
     this.operatingSystem=operatingSystem;
-    this.comparePrice=function(computer){
-        if(this.price>computer.price){
+}
+Computer.prototype.comparePrice=function(computer2){
+        if(this.price>computer2.price){
+            console.log("Purviq komputer e po-skup ot vtoriq");
             return -1;
-        } else if (this.price<computer.price){
+        } else if (this.price<computer2.price){
+            console.log("Vtoriq komputer e po-skup ot purviq");
             return 1;
         } else {
+            console.log("Cenite na dvata komputara sa ravni!");
             return 0;
         }
-    }
 }
-var computer1=new Computer(1996,1000,true,20,10, "windows");
+var computer1=new Computer(1996,1100,true,20,10, "windows");
 var computer2=new Computer(1999,1000,false,40 ,12, "Mac");
-if(computer1.comparePrice(computer2)==-1){
-    console.log("Purviq komputer e po-skup");
-} else if(computer1.comparePrice(computer2)==1){
-    console.log("Vtoriq komputer e po-skup");
-} else {
-    console.log("Cenite na dvata komputara sa ravni!");
-}
+computer1.comparePrice(computer2);
